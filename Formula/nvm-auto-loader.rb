97667e00dbfa3633bf7156602b86f5855558b15f
woongbae-ra <129053689+woongbae-ra@users.noreply.github.com>
2023-10-31 16:03:25 +0900

    fix script for .rb

--- Formula/nvm-auto-loader.rb
+++ Formula/nvm-auto-loader.rb
@@ -8,19 +8,25 @@ class NvmAutoLoader < Formula
   end
 
   def post_install
+    scripts = "source #{opt_prefix}/bin/nvm-auto-loader.sh"
+    
     # For ~/.zshrc
     zshrc_path = Pathname.new("#{ENV["HOME"]}/.zshrc")
-    if zshrc_path.exist? && !zshrc_path.read.include?("source #{opt_prefix}/bin/nvm-auto-loader.sh")
-      system "echo", "'source #{opt_prefix}/bin/nvm-auto-loader.sh'", ">>", zshrc_path
+    if zshrc_path.exist? && !zshrc_path.read.include?(scripts)
+      zshrc_path.open('a') do |file|
+        file.puts scripts
+      end
     end
 
     # For ~/.bash_profile
     bash_profile_path = Pathname.new("#{ENV["HOME"]}/.bash_profile")
-    if bash_profile_path.exist? && !bash_profile_path.read.include?("source #{opt_prefix}/bin/nvm-auto-loader.sh")
-      system "echo", "'source #{opt_prefix}/bin/nvm-auto-loader.sh'", ">>", bash_profile_path
+    if bash_profile_path.exist? && !bash_profile_path.read.include?(scripts)
+      bash_profile_path.open('a') do |file|
+        file.puts scripts
+      end
     end
 
-    echo "nvm-auto-loader is installed successfully! Please restart your terminal."
+    puts "nvm-auto-loader is installed successfully! Please restart your terminal."
   end
   
   test do

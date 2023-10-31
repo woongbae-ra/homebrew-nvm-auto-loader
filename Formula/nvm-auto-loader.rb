e2c06ee5704fcec2ed11d8122aec955bfc925a28
woongbae-ra <129053689+woongbae-ra@users.noreply.github.com>
2023-10-31 16:19:48 +0900

    v1.0.0

--- Formula/nvm-auto-loader.rb
+++ Formula/nvm-auto-loader.rb
@@ -8,11 +8,12 @@ class NvmAutoLoader < Formula
   end
 
   def post_install
-    # Formatting with color
     green = "\033[1;32m"
     reset = "\033[0m"
     bold = "\033[1m"
+    separator = "\033[1;34m" + "-"*80 + "\033[0m"
   
+    puts separator
     puts "\n#{green}🎉  nvm-auto-loader has been successfully installed! #{reset}\n\n"
     puts "#{bold}To finalize the installation, follow these guidelines:#{reset}\n\n"
     
@@ -22,9 +23,6 @@ class NvmAutoLoader < Formula
     puts "2️⃣  Restart your terminal or run the following commands based on your shell:\n\n"
     puts "     #{green}source ~/.zshrc#{reset} (for zsh users)\n"
     puts "     #{green}source ~/.bash_profile#{reset} (for bash users)\n\n"
-  end
-  
-  test do
-    # Here you can define tests for your utility.
+    puts separator
   end
 end
